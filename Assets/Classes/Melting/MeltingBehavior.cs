3cc5bc824b6dc13ff613ebbebff25ede9f8c811e
Baris Demirkap <barisdemirkap@gmail.com>
2022-04-26 20:27:45 +0300

    Add decreasing end behavior

--- Assets/Classes/Melting/MeltingBehavior.cs
+++ Assets/Classes/Melting/MeltingBehavior.cs
@@ -7,6 +7,8 @@ public class MeltingManager : MonoBehaviour
 {
     [SerializeField] private float meltingConstant = 10.0f;
 
+    private bool DecreasingState = true;
+    
         // Start is called before the first frame update
     void Start()
     {
@@ -20,20 +22,42 @@ public class MeltingManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        DecreaseSize();
+    {   
+        CheckMinThicknessAboveZero();
+        
+        if (DecreasingState)
+        {
+            DecreaseSize();
+        }
+        
+        
     }
 
     void DecreaseSize()
     {
         transform.localScale =
-            new Vector3(transform.localScale.x-Time.deltaTime*meltingConstant, transform.localScale.y-Time.deltaTime*meltingConstant, transform.localScale.z-Time.deltaTime*meltingConstant);
+            new Vector3(transform.localScale.x, transform.localScale.y-Time.deltaTime*meltingConstant, transform.localScale.z);
     }
 
     void NormalizePlayerPosition()
     {
         transform.position = new Vector3(transform.position.x, transform.localScale.y / 2, transform.position.z);
     }
+
+    void StopDecreasing()
+    {
+        DecreasingState = false;
+        
+    }
     
+    void CheckMinThicknessAboveZero()
+    {
+        if (transform.localScale.y < 0)
+        {
+            StopDecreasing();
+        }
+    }
+
+
     //Defeat scene eklendikten sonra defeat behavior yaratilacak
 }
