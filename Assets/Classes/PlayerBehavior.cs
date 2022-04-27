3ec3437235e195f6e9adb25e5614d9fe5fc20833
Baris Demirkap <barisdemirkap@gmail.com>
2022-04-27 08:12:23 +0300

    Add forward speed to the player

--- Assets/Classes/PlayerBehavior.cs
+++ Assets/Classes/PlayerBehavior.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerBehavior : MonoBehaviour
 {
+    [SerializeField] private float ForwardSpeed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveDirectionZ();
+    }
+
+
+    void MoveDirectionZ()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime * ForwardSpeed);
     }
 }
