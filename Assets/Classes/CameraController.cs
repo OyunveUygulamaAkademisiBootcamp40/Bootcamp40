59ddf6b8b28027741262176602181d83dfdb679a
Baris Demirkap <barisdemirkap@gmail.com>
2022-05-02 12:05:06 +0300

    feat(camera): Add camera controller

--- Assets/Classes/CameraController.cs
+++ Assets/Classes/CameraController.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject FollowingObject;
+
+    [SerializeField] public float FollowingDistance = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    transform.position = new Vector3(transform.position.x,transform.position.y ,FollowingObject.transform.position.z - FollowingDistance);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3( transform.position.x, transform.position.y,FollowingObject.transform.position.z - FollowingDistance);
     }
 }
