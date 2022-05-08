1134149d2b7e93a3a3b2b0da0662ceb05ffb8deb
Baris Demirkap <barisdemirkap@gmail.com>
2022-05-08 22:47:30 +0300

    Push features
    After conflict, some of the features were backed up which is now pushed again

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    [SerializeField] private float ForwardSpeed = 10.0f;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float XAxisMovementFactor = 5f;
+    [SerializeField] private float ZAxisMovementFactor = 5f;
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 4f)
+        {
+            transform.position = new Vector3(transform.position.x + XAxisMovementFactor * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -4f)
+        {
+            transform.position = new Vector3(transform.position.x - XAxisMovementFactor * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Move()
     {
-        MoveDirectionZ();
+        transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + ZAxisMovementFactor*Time.deltaTime);
     }
 
-
-    void MoveDirectionZ()
-    {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime * ForwardSpeed);
-    }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -10,5 +10,9 @@ public class InputManager : MonoBehaviour
         
     }
 
-  
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
