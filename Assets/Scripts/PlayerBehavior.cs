43e86ff539f2b540b401e93cda987071702987ca
Baris Demirkap <barisdemirkap@gmail.com>
2022-05-09 00:39:37 +0300

    Refactor player movement and melting behaviour

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -5,25 +6,88 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+   protected static bool gameIsStarted = false;
+   protected static bool gameIsFinished = false;
+   private bool moveLeft = false;
+   private bool moveRight = false;
+   
+   
+   
     [SerializeField] private float XAxisMovementFactor = 5f;
     [SerializeField] private float ZAxisMovementFactor = 5f;
+    
+    
     private void Update()
     {
+        GetInput();
+        
+        MoveDirectionZ();
+        MoveDirectionX();
+    }
+
+    void MoveDirectionZ()
+    {
+        if (gameIsStarted && !gameIsFinished)
+        {
+            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + ZAxisMovementFactor*Time.deltaTime);
+        }
+    }
+
+    void GetInput()
+    {
+
         if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 4f)
         {
-            transform.position = new Vector3(transform.position.x + XAxisMovementFactor * Time.deltaTime, transform.position.y, transform.position.z);
+            moveRight = true;
+            if (!gameIsStarted)
+            {
+                StartLevel();
+            }
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -4f)
+        else if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -4f)
+        {
+            moveLeft = true;
+            if (!gameIsStarted)
+            {
+                StartLevel();
+            }
+        }
+        else
+        {
+            moveLeft = false;
+            moveRight = false;
+        }
+
+        
+    }
+
+    void MoveDirectionX()
+    {
+        if (moveLeft && !gameIsFinished)
         {
             transform.position = new Vector3(transform.position.x - XAxisMovementFactor * Time.deltaTime, transform.position.y, transform.position.z);
         }
-
-        Move();
+        if (moveRight && !gameIsFinished)
+        {
+            transform.position = new Vector3(transform.position.x + XAxisMovementFactor * Time.deltaTime, transform.position.y, transform.position.z);
+        }
     }
 
-    void Move()
+    void EndLevel()
     {
-        transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + ZAxisMovementFactor*Time.deltaTime);
+        gameIsFinished = true;
     }
 
+    void StartLevel()
+    {
+        gameIsStarted = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            EndLevel();
+        }
+    }
 }
