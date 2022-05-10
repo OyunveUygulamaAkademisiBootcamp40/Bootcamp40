9af2f5fc2f59f4de784c6f0f7a18c1aafed648d6
Baris Demirkap <barisdemirkap@gmail.com>
2022-05-11 01:02:18 +0300

    Create input manager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class InputManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private float mouseFirstPositionX;
+    public float getInput()
     {
-        
+        setOrigin();
+        return getDifferencefromNewPositiontoOldPosition();
     }
 
-  
+    private void setOrigin()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseFirstPositionX = Input.mousePosition.x;
+        }
+    }
+
+    private float getDifferencefromNewPositiontoOldPosition()
+    {
+        if (mouseFirstPositionX != null && Input.GetMouseButton(0))
+        {
+            return Input.mousePosition.x - mouseFirstPositionX;
+        }
+
+        return 0;
+    }
+
+
 }
