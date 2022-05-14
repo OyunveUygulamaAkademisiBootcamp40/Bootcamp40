a59600423bf0ba64ed055966db67048a7d56ec72
mustafaozger <ozgermustafa118@gmail.com>
2022-05-14 17:20:52 +0300

    Coin collection animation made

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
+   [SerializeField] Animator _anim;
+    public Text _coinCountText;
+    private float _coinCount;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Coin"))
         {
-            Destroy(gameObject);
+            Destroy(other.gameObject);
+            _coinCount++;
+            _coinCountText.text = _coinCount.ToString();
             TotalScore._totalScore++;
-
+           _anim.SetBool("coinControl", true);
+            StartCoroutine(Wait());
         }
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.4f);
+        _anim.SetBool("coinControl", false);
+        
     }
 }
--- Assets/Scripts/TotalScore.cs
+++ Assets/Scripts/TotalScore.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class TotalScore : MonoBehaviour
 {
     public static int _totalScore;
+    
+ 
 }
