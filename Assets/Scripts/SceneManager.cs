0b9f71784f26900df6bbbc86b1ff6a7f7a772c27
Alpeen <alpeen@outlook.com.tr>
2022-05-24 17:29:06 +0300

    Added Level Looping

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,9 +5,22 @@ using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 
 public class SceneManager : MonoBehaviour
 {
+    public static int playerLevel = 1;
+
+
     public void OtherScene()
     {
-        UnitySceneManager.LoadScene(UnitySceneManager.GetActiveScene().buildIndex + 1);
+        playerLevel++;
+        if (playerLevel%3==0)
+        {
+            UnitySceneManager.LoadScene(0);
+        }
+        else
+        {
+            UnitySceneManager.LoadScene(UnitySceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+
     }
     public void RetryScene()
     {
