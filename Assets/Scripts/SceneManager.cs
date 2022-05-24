8b188a901c5d415fd68874dce401e0689e140d60
Alpeen <alpeen@outlook.com.tr>
2022-05-24 16:03:38 +0300

    Level3 & Scene Manager Added

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OtherScene()
     {
-        
+        UnitySceneManager.LoadScene(UnitySceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void RetryScene()
     {
-        
+        UnitySceneManager.LoadScene(UnitySceneManager.GetActiveScene().buildIndex);
     }
 }
