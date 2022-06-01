6d60cd9256beb350ea14850a1675c547bdec50af
Alpeen <alpeen@outlook.com.tr>
2022-06-01 16:36:04 +0300

    PlayFab Package added & Playfab Manager Script created

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    void Start()
+    {
+        Login();
+    }
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successfull login/ Account created!");
+    }
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while logging in/ creating account!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
+}
